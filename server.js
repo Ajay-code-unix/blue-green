2342f9a9c3ba80f06d2ec60e23cebb3e47b8d386
Ajay-code-unix <58654765+Ajay-code-unix@users.noreply.github.com>
2024-11-08 09:59:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app = express();
 const PORT=3000;
-app.get('/',(req,res)=> res.send('blue'));
+app.get('/',(req,res)=> res.send('greenn'));
 app.listen(PORT,()=>console.log(`APP running on Port ${PORT}`));
 
